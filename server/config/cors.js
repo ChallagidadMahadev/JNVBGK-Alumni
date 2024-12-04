8ba59b94fcd76537885880b160825bd29be31cc6
ChallagidadMahadev <madhavan863@gmail.com>
2024-12-04 18:56:36 +0530

    server urls
    updated all server urls

--- server/config/cors.js
+++ server/config/cors.js
@@ -1,7 +1,10 @@
 import cors from "cors";
 
 const corsOptions = {
-  origin: true, // Allow all origins
+  origin:
+    process.env.NODE_ENV === "production"
+      ? "https://frolicking-moonbeam-d09148.netlify.app"
+      : "http://localhost:5173",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
